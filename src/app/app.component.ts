57043796e8692d5483a3638349a4354b9d40fc62
mark953fps <markville953@gmail.com>
2021-11-25 14:00:04 +0800

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  public data: any = {
-    deviceName: 'NW01',
-    documentId: '62ea50e5-7a8b-4b48-95bf-af1b8b72ff67',
-    documentName: 'wallpaper_rise_17_62ea50e5-7a8b-4b48-95bf-af1b8b72ff67.jpg',
-    documentType: 'Patron Identification',
-  };
+  public data: any = [
+    {
+      deviceName: 'NW01',
+      documentId: '62ea50e5-7a8b-4b48-95bf-af1b8b72ff67',
+      documentName:
+        'wallpaper_rise_17_62ea50e5-7a8b-4b48-95bf-af1b8b72ff67.jpg',
+      documentType: 'Patron Identification',
+    },
+  ];
 
   public ngOnInit() {}
 
