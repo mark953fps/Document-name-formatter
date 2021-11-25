8ce3f3082759c784359a89ea67de28f84cac5900
mark953fps <markville953@gmail.com>
2021-11-25 14:02:15 +0800

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,13 @@ export class AppComponent implements OnInit {
         'wallpaper_rise_17_62ea50e5-7a8b-4b48-95bf-af1b8b72ff67.jpg',
       documentType: 'Patron Identification',
     },
+    {
+      deviceName: 'NW01',
+      documentId: '62ea50e5-7a8b-4b48-95bf-af1b8b72ff67',
+      documentName:
+        'markvillegas_test_18_62ea50e5-7a8b-4b48-95bf-af1b8b72ff67.jpg',
+      documentType: 'Patron Identification',
+    },
   ];
 
   public ngOnInit() {}
