3de7555aeca6585d5e4cc378deaf08c60c015bc3
mark953fps <markville953@gmail.com>
2021-11-25 13:46:05 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-fnmeve
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-fnmeve)
---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  public data: any = {
+    deviceName: 'NW01',
+    documentId: '62ea50e5-7a8b-4b48-95bf-af1b8b72ff67',
+    documentName: 'wallpaper_rise_17_62ea50e5-7a8b-4b48-95bf-af1b8b72ff67.jpg',
+    documentType: 'Patron Identification',
+  };
+
+  public ngOnInit() {}
+
+  public documentNameFormat(data: any): any {
+    return data.documentName.replace(`_${data.documentId}`, '');
+  }
+}
